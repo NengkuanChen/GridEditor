a7ddf8057163b41551c67071e3a49f41c5a4fe2c
Fiiction <Kai_Jin_1998@163.com>
2024-10-14 10:51:55 -0400

    In-Game changing tiles

--- Assets/Scripts/Grids/TestGridScript.cs
+++ Assets/Scripts/Grids/TestGridScript.cs
@@ -1,28 +1,31 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 namespace Grids
 {
-    public class TestGridScript : GridScript
+    public class TestGridScript: GridScript
     {
         
         string[] gridString = new string[]{
             //RE_MARK_0_START
-            "-ww-|-wr-rrrr-|---rrrr-----",
-            "-ww-|----r----|------r--www",
-            "-ww-|----r----|------r--ww-",
-            "-ww-|----rrrrrrrrrrrr--dww-",
-            "-wwwwwwww|----|-------dww--",
-            "-wwwwwwwwww---|------d-----",
-            "----|---www---|-----dd-----",
-            "----|---www---|----dd------",
-            "--ddd----w--www---dd-------",
-            "--drd----wwww-w--dd--------",
-            "--drd----|----wwdd---------",
-            "--dddddd-|----|wdd---------",
-            "----dddddd----|------------",
-            "----|---dd----|------------",
-            //RE_MARK_0_END
+			"-ww-|-wr-rrrr-|---rrrr-----",
+			"-ww-|----r----|------r--www",
+			"-ww-|----r----|------r--ww-",
+			"-ww-|----rrrrrrrrrrrr--dww-",
+			"-wwwwwwwwdddd-ddd-----dww--",
+			"-wwwwwwwwdw---|--ddd-d-----",
+			"----|---wdw---|-----dd-----",
+			"----|---wwddd-|ddd-dd-ddd--",
+			"--ddd----w--www---dd--d-d--",
+			"--drd----wwww-w--wdd--d-dd-",
+			"--drd----|----wwdd-d-d--dd-",
+			"--dddddd-|----|wdd-ddd-d-d-",
+			"----dddddd----|-----d-dd-d-",
+			"----|---dd----|------------",
+			//RE_MARK_0_END
+
         };
 
         private Dictionary<char, Material> materialDictionary = new Dictionary<char, Material>();
@@ -30,23 +33,25 @@ namespace Grids
         private Dictionary<char, string> materialPathDictionary = new Dictionary<char, string>()
         {
             //RE_MARK_1_START
-            {'w', "_Core/Materials/Water"},
-            {'r', "_Core/Materials/Rock"},
-            {'-', "_Core/Materials/Grass"},
-            {'d', "_Core/Materials/Rock"},
-            {'|', "_Core/Materials/Forest"},
-            //RE_MARK_1_END
+			{'w', "_Core/Materials/Water"},
+			{'r', "_Core/Materials/Rock"},
+			{'-', "_Core/Materials/Grass"},
+			{'d', "_Core/Materials/Rock"},
+			{'|', "_Core/Materials/Forest"},
+			//RE_MARK_1_END
+
         };
         
         private Dictionary<char, float> costDictionary = new Dictionary<char, float>()
         {
             //RE_MARK_2_START
-            {'w', 1.34f},
-            {'r', 1},
-            {'d', 10},
-            {'-', 1},
-            {'|', 1},
-            //RE_MARK_2_END
+			{'w', 1.34f},
+			{'r', 1f},
+			{'-', 1f},
+			{'d', 10f},
+			{'|', 1f},
+			//RE_MARK_2_END
+
         };
 
         // Use this for initialization
@@ -54,8 +59,46 @@ namespace Grids
             LoadMaterials();
             gridWidth = gridString[0].Length;
             gridHeight = gridString.Length;
+            gridArray = GetGrid();
+            Debug.Log($"width: {gridWidth}, height: {gridHeight}");
         }
 
+        void UpdateTile(int x, int y)
+        {
+	        Destroy(gridArray[x, y]);
+	        
+	        float offsetX = (gridWidth  * -spacing)/2f;
+	        float offsetY = (gridHeight * spacing)/2f;
+	        
+	        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+	        quad.transform.localScale = new Vector3(spacing, spacing, spacing);
+	        quad.transform.position = new Vector3(offsetX + x * spacing, 
+		        offsetY - y * spacing, 0);
+
+	        quad.transform.parent = transform;
+
+	        gridArray[x, y] = quad;
+					
+	        quad.GetComponent<MeshRenderer>().sharedMaterial = GetMaterial(x, y);
+        }
+
+        // Change the tile on (x, y) according to the char indicator
+        public void ChangeTile(int x, int y, char indicator)
+        {
+	        StringBuilder sb = new StringBuilder(gridString[y]);
+	        sb[x] = indicator;
+	        gridString[y] = sb.ToString();
+	        UpdateTile(x, y);
+        }
+        
+        // Change the tile on (x, y) to a random material
+        public void RandomChangeTile(int x, int y)
+        {
+	        var keys = materialDictionary.Keys.ToArray();
+	        char key = keys[Random.Range(0, keys.Length)];
+	        ChangeTile(x, y, key);
+        }
+        
         public void LoadMaterials()
         {
             foreach (var pair in materialPathDictionary)
